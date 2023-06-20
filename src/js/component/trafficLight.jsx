ad5ebfec223b5b4ef6753ed1a4090cadbf22940e
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-06-20 17:10:24 +0000

    Aplicados Bonus

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -4,16 +4,22 @@ import React, { useState } from 'react';
 const TrafficLight = () => {
 
 	const [color, setColor] = useState("red");
+	const [show, setShow] = useState(true);	
 
 	const party = () => {
 		color === "red" 
 			? setColor("orange") 
 			: color === "orange" 
 			? setColor("green") 
-			: color === "green" 
-			? setColor("red") 
+			: color === "green" && show === true 
+			? setColor("purple") 
+			: color === "green" && show === false 
+			? setColor("red")
 			: null 
 	}
+
+	console.log(show)
+	console.log(color)
 
 	return (
 		<div>
@@ -22,7 +28,13 @@ const TrafficLight = () => {
 				<div className={color === "red"? "trafficLightRed shine" : "trafficLightRed"} onClick={() => setColor("red")}></div>
 				<div className={color === "orange"? "trafficLightOrange shine" : "trafficLightOrange"} onClick={() => setColor("orange")}></div>
 				<div className={color === "green"? "trafficLightGreen shine" : "trafficLightGreen"} onClick={() => setColor("green")}></div>
+				<div className={color === "purple" && show === true
+					? "trafficLightPurple shine"
+					: color !== "purple" && show === true
+					? "trafficLightPurple" 
+					:"hide" } onClick={() => setColor("purple")}></div>
 				<button type="button" onClick={party}>Party</button>
+				<button type="button" onClick={() => setShow(!show)}>Extra</button>
 			</div>
 		</div>
 	)
